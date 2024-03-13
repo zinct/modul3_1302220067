a0ea202c115caec83ee1d2e622d71e9196a7e1bb
Indra Mahesa <indramahesa128@gmail.com>
2024-03-13 13:16:16 +0700

    menambahkan class PosisiKarakterGame

--- modul3_1302220067/Program.cs
+++ modul3_1302220067/Program.cs
@@ -22,9 +22,78 @@
         }
     }
 
+    public enum PosisiKarakterGameState { TENGKURAP, JONGKOK, BERDIRI, TERBANG };
+    public enum Trigger { TOMBOL_W, TOMBOL_S, TOMBOL_X };
+
+    public class PosisiKarakterGame
+    {
+        public PosisiKarakterGameState currentState = PosisiKarakterGameState.BERDIRI;
+
+        public class Transition
+        {
+
+            public PosisiKarakterGameState stateAwal;
+            public PosisiKarakterGameState stateAkhir;
+            public Trigger trigger;
+            public Transition(PosisiKarakterGameState stateAwal, PosisiKarakterGameState stateAkhir, Trigger trigger)
+            {
+                this.stateAwal = stateAwal;
+                this.stateAkhir = stateAkhir;
+                this.trigger = trigger;
+            }
+        }
+
+        Transition[] transisi =
+        {
+            new Transition(PosisiKarakterGameState.TENGKURAP, PosisiKarakterGameState.JONGKOK, Trigger.TOMBOL_W),
+            new Transition(PosisiKarakterGameState.JONGKOK, PosisiKarakterGameState.TENGKURAP, Trigger.TOMBOL_S),
+            new Transition(PosisiKarakterGameState.JONGKOK, PosisiKarakterGameState.BERDIRI, Trigger.TOMBOL_W),
+            new Transition(PosisiKarakterGameState.BERDIRI, PosisiKarakterGameState.JONGKOK, Trigger.TOMBOL_S),
+            new Transition(PosisiKarakterGameState.BERDIRI, PosisiKarakterGameState.TERBANG, Trigger.TOMBOL_W),
+            new Transition(PosisiKarakterGameState.TERBANG, PosisiKarakterGameState.BERDIRI, Trigger.TOMBOL_S),
+            new Transition(PosisiKarakterGameState.TERBANG, PosisiKarakterGameState.JONGKOK, Trigger.TOMBOL_X),
+        };
+
+        public PosisiKarakterGameState GetNextState(PosisiKarakterGameState stateAwal, Trigger trigger)
+        {
+            PosisiKarakterGameState stateAkhir = stateAwal;
+
+            for (int i = 0; i < transisi.Length; i++)
+            {
+                Transition perubahan = transisi[i];
+
+                if (stateAwal == perubahan.stateAwal && trigger == perubahan.trigger)
+                {
+                    stateAkhir = perubahan.stateAkhir;
+                }
+            }
+
+            return stateAkhir;
+        }
+
+        public void ActivateTrigger(Trigger trigger)
+        {
+            PosisiKarakterGameState stateAwal = currentState;
+            currentState = GetNextState(currentState, trigger);
+
+            // Event when state changes
+            if(stateAwal == PosisiKarakterGameState.TERBANG && currentState == PosisiKarakterGameState.JONGKOK)
+            {
+                Console.WriteLine("Posisi Landing");
+            }
+
+            if(stateAwal == PosisiKarakterGameState.BERDIRI && currentState == PosisiKarakterGameState.TERBANG)
+            {
+                Console.WriteLine("Posisi Take Off");
+            }
+        }
+    }
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Buah Alpukat merupakan kode : " + KodeBuah.getKodeBuah(NamaBuah.Alpukat));
-        Console.WriteLine("Buah Paprika merupakan kode : " + KodeBuah.getKodeBuah(NamaBuah.Paprika));
+        PosisiKarakterGame game = new PosisiKarakterGame();
+        Console.WriteLine("State Awal Sedang Berdiri");
+        game.ActivateTrigger(Trigger.TOMBOL_W);
+        game.ActivateTrigger(Trigger.TOMBOL_X);
     }
 }
